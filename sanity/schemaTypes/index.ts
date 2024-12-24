89a3065e293b0bbb6f655bbee61ef24a691e4f3d
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2024-12-25 00:11:06 +0530

    code formated

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { author } from './author'
+import { type SchemaTypeDefinition } from 'sanity';
+import { author } from './author';
 import { startup } from './startup';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
